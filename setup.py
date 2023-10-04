40b112dd070e6b009214572a3d0d0facba238c58
Blake Dewey <blake.dewey@jhu.edu>
2023-10-04 10:29:57 -0400

    add util func for affine update. update setup.py

--- setup.py
+++ setup.py
@@ -7,22 +7,26 @@ with open(os.path.join(dir, 'README.md')) as f:
     long_description = f.read()
 
 setup(name='resize',
-      version='0.1.3',
-      author='Shuo Han',
+      version='0.2.0',
+      author='IACL',
       description='Resize an image with correct sampling coordinates.',
       long_description=long_description,
       long_description_content_type='text/markdown',
-      author_email='shuo_han@outlook.com',
-      url='https://github.com/shuohan/resize',
+      author_email='blake.dewey@jhu.edu',
+      url='https://github.com/iacl/resize',
       license='MIT',
       packages=find_packages(),
-      install_requires=['numpy'],
+      install_requires=[
+            'numpy',
+            'scipy',
+            'transforms3d',
+      ],
       extras_require={
-            'scipy': 'scipy',
-            'pytorch': 'torch>=1.10.0'
+            'pytorch': 'torch>=1.10.0',
       },
       python_requires='>=3.7',
       include_package_data=True,
       classifiers=['Programming Language :: Python :: 3',
                    'License :: OSI Approved :: MIT License',
-                   'Operating System :: OS Independent'])
+                   'Operating System :: OS Independent']
+)
