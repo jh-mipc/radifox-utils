643a6e0e25c9e974cf0816f3eb76553cd28b9b4d
Blake Dewey <blake.dewey@jhu.edu>
2023-10-04 14:18:53 -0400

    fix gitattributes and docs

--- resize/affine.py
+++ resize/affine.py
@@ -4,11 +4,13 @@ from transforms3d.affines import decompose, compose
 
 def update_affine(affine, scales):
     """Updates affine matrix to take into account new resolution
+
     Args:
         affine (numpy.ndarray): The affine matrix to update.
         scales (tuple[float] or list[float]): Resolution scales in each direction.
             Less than 1 for upsampling. For example, ``(2.0, 0.8)`` for a 2D image
             and ``(1.3, 2.1, 0.3)`` for a 3D image.
+
     """
     # Decompose input affine
     tranforms, rotation, zooms, shears = decompose(affine)
