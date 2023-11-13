45699a99d439f88c75d6f07186278a252fed0f07
Blake Dewey <blake.dewey@jhu.edu>
2023-11-13 14:04:11 -0500

    merge degrade and resize repos together

--- degrade-src/README.md
+++
@@ -1,4 +0,0 @@
-# Degrade
-
-This is a small library to degrade (blur and downsample) a signal. Here, we
-aim to properly model the forward process in signal acquisition.
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-import os
+from pathlib import Path
 from setuptools import setup, find_packages
+
+
+__package_name__ = "radifox-utils"
 
 
 def get_version_and_cmdclass(pkg_path):
@@ -16,36 +19,45 @@ def get_version_and_cmdclass(pkg_path):
     return module.__version__, module.get_cmdclass(pkg_path)
 
 
-version, cmdclass = get_version_and_cmdclass("resize")
-
-with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")) as f:
-    long_description = f.read()
+__version__, cmdclass = get_version_and_cmdclass(__package_name__)
 
 setup(
-    name="resize",
-    version=version,
+    name=__package_name__,
+    version=__version__,
     cmdclass=cmdclass,
     author="IACL",
-    description="Resize an image with correct sampling coordinates.",
-    long_description=long_description,
+    description="Medical image utilities provided with RADIFOX.",
+    long_description=(Path(__file__).parent.resolve() / "README.md").read_text(),
     long_description_content_type="text/markdown",
-    author_email="blake.dewey@jhu.edu",
-    url="https://github.com/iacl/resize",
-    license="MIT",
+    author_email="iaclsoftware@jhu.edu",
+    url="https://github.com/iacl/radifox-utils",
+    license="Apache License, 2.0",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "apply-degrade=degrade.main:main",
+            "remove-3D-inplane-interp=degrade.downsample_3D_inplane:main",
+        ]
+    },
     install_requires=[
+        "nibabel",
         "numpy",
         "scipy",
+        "sigpy",
         "transforms3d",
     ],
     extras_require={
+        "dev": [
+            "pytest",
+            "pytest-cov",
+        ],
         "pytorch": "torch>=1.10.0",
     },
     python_requires=">=3.7",
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
 )
